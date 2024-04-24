f8f7747b0a5126ab2aaeb08604eca38109727b92
Rachy <RachyHassan@github.com>
2024-04-24 19:14:36 +0100

    2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,6 +1,9 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
+  return promise.then(() => {
     console.log('Got a response from the API');
     return ({ status: 200, body: 'Success' });
-  }).catch(() => (Error()));
+  }).catch(() => {
+    console.error('Got an error from the API');
+    return Error();
+  });
 }
