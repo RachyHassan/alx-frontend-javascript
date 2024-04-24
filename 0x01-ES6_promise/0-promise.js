18865893f8a7f510a256926baa2648b7a451acd5
Rachy <RachyHassan@github.com>
2024-04-24 15:17:20 +0100

    1-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,4 @@
 export default function getResponseFromAPI() {
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       const response = 'Some response from the API';
-//       resolve(response);
-//     }, 1000);
-//   });
   return new Promise((resolve, reject) => {
     const a = 2 + 3;
     if (a === 5) {
