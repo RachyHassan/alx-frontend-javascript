62e25135044fda3ffbee694a3e508f1f1d8de7c4
Rachy <RachyHassan@github.com>
2024-04-24 19:24:52 +0100

    3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,5 +4,5 @@ export default function handleProfileSignup() {
   Promise.all([uploadPhoto(), createUser()])
     .then(([photo, user]) => {
       console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
-    }).catch(() => (Error(console.log('Signup system offline'))));
+    }).catch(() => Promise.reject(Error('Signup system offline')));
 }
