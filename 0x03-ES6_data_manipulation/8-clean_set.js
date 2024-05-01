b2e7b754afd83fbcd9871551a643a96e22bf2edf
Rachy <RachyHassan@github.com>
2024-05-01 04:53:48 +0100

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,3 +1,12 @@
-// export default function cleanSet(set, startString) {
+export default function cleanSet(set, startString) {
+  let value = '';
+  const newSet = new Set();
 
-// }
+  for (const s of set) {
+    if (s.startsWith(startString)) {
+      newSet.add(s.substring(startString.length));
+    }
+  }
+  value = Array.from(newSet).join('-');
+  return value;
+}
